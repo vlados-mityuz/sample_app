fff3352ba74321d80f220040e37412df3fcf6107
Vladislav <thenoma1@gmail.com>
2021-04-05 23:24:45 +0300

    Links of header change

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id]=user.id
   end
+
+  #returns true if user is logged in or return else if is not
+  def logged_in?
+    !current_user.nil?
+  end
+
+  #returns true if current user is logged in
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
