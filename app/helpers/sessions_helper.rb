26a92523f0f580fe3fc28eecabc0cf5aec099118
Vladislav <thenoma1@gmail.com>
2021-04-06 19:55:09 +0300

    Add log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,4 +14,10 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  # logs out current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,7 +15,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert flash.empty?
   # end
 
-  # test "login with valid information" do
+  # test "login with valid information followed by logout" do
   #   get login_path
   #   post login_path, session: {email: @user.email, password: 'password'}
   #   assert_redirected_to @user
@@ -24,5 +24,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert_select "a[href=?]", login_path, count:0
   #   assert_select "a[href=?]", logout_path
   #   assert_select "a[href=?]", user_path(@user)
+  # delete logout_path
+  # assert_not is_logged_in?
+  # assert_redirected_to root_url
+  # follow_redirect!
+  #   assert_select "a[href=?]", login_path
+  #   assert_select "a[href=?]", logout_path, count:0
+  #   assert_select "a[href=?]", user_path(@user), count:0
   # end
 end
