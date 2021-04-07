29f91c1ccfefb69c754bb91832ca7b915042ac89
Vladislav <thenoma1@gmail.com>
2021-04-07 20:31:22 +0300

    Remember users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,26 @@ module SessionsHelper
     session[:user_id]=user.id
   end
 
+  # Remembers user in permanent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns user with specified token from cookies
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.autheticated?(cookies[:remember_token])
+        log_in user_id
+        @current_user = user
+      end
+    end
+  end 
+
   #returns true if user is logged in or return else if is not
   def logged_in?
     !current_user.nil?
