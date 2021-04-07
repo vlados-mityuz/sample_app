4068385563d0c87af46210081c3bca01012d278a
Vladislav <thenoma1@gmail.com>
2021-04-07 20:51:34 +0300

    Finish remember and forget users

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
   # Returns true if specified token equals digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
