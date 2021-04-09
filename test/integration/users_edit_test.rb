024e30fe20554016834f4e0a1c509e233965908e
Vladislav <thenoma1@gmail.com>
2021-04-09 21:31:08 +0300

    Handling successful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,7 +8,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), user: {name: '', email 'foo@invalid', password: 'foo', confirm_password: 'bar'}
+    patch user_path(@user), user: {name: '', email 'foo@invalid', password: 'foo', password_confirmation: 'bar'}
     assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: {name: name, email: email, password: "", password_confirmation: ""}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
   end
 end
