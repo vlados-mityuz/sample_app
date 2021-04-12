5f85629afd90d9d151345b2d303a976002236dfa
Vladislav <thenoma1@gmail.com>
2021-04-12 21:27:37 +0300

    Require users login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -38,4 +40,12 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    # Pre-filters
+    # Confirms user is logged in
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,25 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:vlas)
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
+  end
+
+  test "should redirect edit when not logged_in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged_in" do
+    patch :update, id: @user, user: {name = @user.name, email: @user.email}
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
