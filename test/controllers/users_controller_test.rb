02fcbd46ce0926a6311db213d0a7977b643b9038
Vladislav <thenoma1@gmail.com>
2021-04-13 20:51:34 +0300

    Add users list

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = = users(:tom)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
