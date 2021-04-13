eac4fa6ec2143d6011d1655f66437afac9aaa024
Vladislav <thenoma1@gmail.com>
2021-04-13 20:15:34 +0300

    Require rights for users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,6 +35,11 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # Returns true if the user is current
+  def current_user?(user)
+    user == current_user
+  end
+
   # Forgets permanent session
   def forget(user)
     user.forget
